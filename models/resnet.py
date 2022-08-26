8976ca43862b0e0d4674fb0d6217eb6e1889d843
vdquang1991 <33130740+vdquang1991@users.noreply.github.com>
2022-08-26 14:50:41 +0800

    fix error

--- models/resnet.py
+++ models/resnet.py
@@ -1,95 +1,121 @@
-
-from __future__ import absolute_import, division, print_function
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
 import torch
-import torch.nn as nn
-from collections import OrderedDict
-import torchvision.models as models
-import torch.utils.model_zoo as model_zoo
-import numpy as np
-import os, math
-from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ResnetEncoder(nn.Module):
-    """Pytorch module for a resnet encoder
-    """
-    def __init__(self, num_layers=34, isPretrained=False, isGrayscale=False, embDimension=128, poolSize=4):
-        super(ResnetEncoder, self).__init__()
-        self.path_to_model = '/tmp/models'
-        self.num_ch_enc = np.array([64, 64, 128, 256, 512])
-        self.isGrayscale = isGrayscale
-        self.isPretrained = isPretrained
-        self.embDimension = embDimension
-        self.poolSize = poolSize
-        self.featListName = ['layer0', 'layer1', 'layer2', 'layer3', 'layer4']
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        resnets = {
-            18: models.resnet18,
-            34: models.resnet34,
-            50: models.resnet50,
-            101: models.resnet101,
-            152: models.resnet152}
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-        resnets_pretrained_path = {
-            18: 'resnet18-5c106cde.pth',
-            34: 'resnet34.pth',
-            50: 'resnet50-19c8e357.pth',
-            101: 'resnet101.pth',
-            152: 'resnet152.pth'}
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
-        if num_layers not in resnets:
-            raise ValueError("{} is not a valid number of resnet layers".format(
-                num_layers))
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-        self.encoder = resnets[num_layers]()
 
-        if self.isPretrained:
-            print("using pretrained model")
-            self.encoder.load_state_dict(
-                torch.load(os.path.join(self.path_to_model, resnets_pretrained_path[num_layers])))
+class Bottleneck(nn.Module):
+    expansion = 4
 
-        if self.isGrayscale:
-            self.encoder.conv1 = nn.Conv2d(
-                1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.num_classes = num_classes
+        if self.num_classes <=100:
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         else:
-            self.encoder.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.fc = nn.Linear(512*block.expansion, num_classes)
 
-        if num_layers > 34:
-            self.num_ch_enc[1:] = 2048
-        else:
-            self.num_ch_enc[1:] = 512
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
 
-        if self.embDimension >0:
-            self.fc =  nn.Linear(self.num_ch_enc[-1], self.embDimension)
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        if self.num_classes > 100:
+            out = self.maxpool(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
-    def forward(self, input_image):
-        self.features = []
+def ResNet18(num_classes):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
 
-        x = self.encoder.conv1(input_image)
-        x = self.encoder.bn1(x)
-        x = self.encoder.relu(x)
-        self.features.append(x)
 
-        x = self.encoder.layer1(x)
-        self.features.append(x)
+def ResNet34(num_classes):
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
 
-        x = self.encoder.layer2(x)
-        self.features.append(x)
 
-        x = self.encoder.layer3(x)
-        self.features.append(x)
+def ResNet50(num_classes):
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes)
 
-        x = self.encoder.layer4(x)
-        self.features.append(x)
 
-        x = F.avg_pool2d(x, self.poolSize)
-        self.x = x.view(x.size(0), -1)
+def ResNet101(num_classes):
+    return ResNet(Bottleneck, [3, 4, 23, 3], num_classes)
 
-        x = self.fc(self.x)
-        return x
+
+def ResNet152(num_classes):
+    return ResNet(Bottleneck, [3, 8, 36, 3], num_classes)
